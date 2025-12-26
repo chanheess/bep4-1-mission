d199f2b7bcbaeecfe1ecae287a0cfac4fad8b901
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-12-26 12:50:00 +0900

    0031

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -2,21 +2,18 @@ package com.back.boundedContext.post.app;
 
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
-import com.back.boundedContext.post.out.PostMemberRepository;
-import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.rsData.RsData;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
-    private final PostMemberRepository postMemberRepository;
     private final PostSupport postSupport;
     private final PostWriteUseCase postWriteUseCase;
     private final PostSyncMemberUseCase postSyncMemberUseCase;
@@ -44,6 +41,11 @@ public class PostFacade {
     @Transactional(readOnly = true)
     public Optional<PostMember> findPostMemberByUsername(String username) {
         return postSupport.findMemberByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Post> findByOrderByIdDesc() {
+        return postSupport.findByOrderByIdDesc();
     }
 
 }
